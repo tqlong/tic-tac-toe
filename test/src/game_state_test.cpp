d3912ac2bd3d5674c16163b0753404589e81ae1d
huyenbui117 <huyenbui117@gmail.com>
2022-12-23 00:07:10 +0700

    done unittest alpha_beta.cpp

--- test/src/game_state_test.cpp
+++ test/src/game_state_test.cpp
@@ -113,6 +113,43 @@ protected:
         }
     }
 
+    void TestGetScoreState(){
+        GameState finalState;
+            finalState.s[0][0] = 'X'; finalState.s[0][1] = 'X'; finalState.s[0][2] = 'X';
+            finalState.s[1][0] = 'X'; finalState.s[1][1] = 'O'; finalState.s[1][2] = 'O';
+            finalState.s[2][0] = ' '; finalState.s[2][1] = 'O'; finalState.s[2][2] = ' ';
+        {
+            GameState state;
+            state = finalState;
+            EXPECT_EQ(state.getScore(state.s, true, 100, -100), std::make_pair(1, state.s));
+        }
+        {
+            GameState state1;
+            state1.s[0][0] = 'X'; state1.s[0][1] = ' '; state1.s[0][2] = 'X';
+            state1.s[1][0] = 'X'; state1.s[1][1] = 'O'; state1.s[1][2] = 'O';
+            state1.s[2][0] = ' '; state1.s[2][1] = 'O'; state1.s[2][2] = ' ';
+            EXPECT_EQ(state1.getScore(state1.s, true, 1, 0), std::make_pair(1, finalState.s));
+        }
+        {
+            GameState state2;
+            state2.s[0][0] = 'X'; state2.s[0][1] = 'X'; state2.s[0][2] = ' ';
+            state2.s[1][0] = 'X'; state2.s[1][1] = 'O'; state2.s[1][2] = 'O';
+            state2.s[2][0] = ' '; state2.s[2][1] = 'O'; state2.s[2][2] = ' ';
+            EXPECT_EQ(state2.getScore(state2.s, false, 1, 1), std::make_pair(1, finalState.s));
+        }
+
+        {
+            GameState state3;
+            state3.s[0][0] = 'X'; state3.s[0][1] = 'X'; state3.s[0][2] = 'O';
+            state3.s[1][0] = 'X'; state3.s[1][1] = 'O'; state3.s[1][2] = ' ';
+            state3.s[2][0] = ' '; state3.s[2][1] = ' '; state3.s[2][2] = ' ';
+            GameState finalState1;
+            finalState1.s[0][0] = 'X'; finalState1.s[0][1] = 'X'; finalState1.s[0][2] = 'O';
+            finalState1.s[1][0] = 'X'; finalState1.s[1][1] = 'O'; finalState1.s[1][2] = ' ';
+            finalState1.s[2][0] = 'O'; finalState1.s[2][1] = ' '; finalState1.s[2][2] = ' ';
+            EXPECT_EQ(state3.getScore(state3.s, false, -100, 100), std::make_pair(-1, finalState1.s));
+        }
+    }
 private:
     GameState state;
 };
@@ -143,4 +180,7 @@ TEST_F(GameStateTest, screen) {
 
 TEST_F(GameStateTest, scoreFinalState) {
     TestScoreFinalState();
+}
+TEST_F(GameStateTest, getScoreState) {
+    TestGetScoreState();
 }
