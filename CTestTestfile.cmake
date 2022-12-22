6df6f965824b1d2cdce68c7f11cac71236d54361
huyenbui117 <huyenbui117@gmail.com>
2022-12-23 00:17:20 +0700

    clear unnecessity files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/huyenbui117/D/AILab/tic-tac-toe
-# Build directory: /media/huyenbui117/D/AILab/tic-tac-toe
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
