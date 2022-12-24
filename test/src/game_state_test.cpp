ca0ebddaea7c44c1f851ce6f5bc178b56e271377
huyenbui117 <huyenbui117@gmail.com>
2022-12-24 16:48:49 +0700

    finished unit testing for alpha_beta.cpp

--- test/src/game_state_test.cpp
+++ test/src/game_state_test.cpp
@@ -81,7 +81,75 @@ protected:
         state.playScreen = GameState::PlayScreen::PlayingScreen;
         EXPECT_EQ(state.playScreen, GameState::PlayScreen::PlayingScreen);
     }
+    
+    void TestScoreFinalState() {
+        {
+            GameState state;
+            state.s[0][0] = 'X'; state.s[0][1] = 'X'; state.s[0][2] = 'X';
+            state.s[1][0] = 'X'; state.s[1][1] = 'O'; state.s[1][2] = 'O';
+            state.s[2][0] = 'O'; state.s[2][1] = ' '; state.s[2][2] = ' ';
+            EXPECT_EQ(state.getScoreFinalState(state.s), 1);
+        }        
+        {
+            GameState state1;
+            state1.s[0][0] = 'X'; state1.s[0][1] = 'O'; state1.s[0][2] = 'X';
+            state1.s[1][0] = 'X'; state1.s[1][1] = 'O'; state1.s[1][2] = 'O';
+            state1.s[2][0] = 'X'; state1.s[2][1] = ' '; state1.s[2][2] = ' ';
+            EXPECT_EQ(state1.getScoreFinalState(state1.s), 1);
+        }
+        {
+            GameState state2;
+            state2.s[0][0] = 'X'; state2.s[0][1] = 'O'; state2.s[0][2] = 'O';
+            state2.s[1][0] = ' '; state2.s[1][1] = 'X'; state2.s[1][2] = 'O';
+            state2.s[2][0] = 'X'; state2.s[2][1] = ' '; state2.s[2][2] = 'X';
+            EXPECT_EQ(state2.getScoreFinalState(state2.s), 1);
+        }
+        {
+            GameState state3;
+            state3.s[0][0] = 'X'; state3.s[0][1] = 'O'; state3.s[0][2] = 'X';
+            state3.s[1][0] = 'O'; state3.s[1][1] = 'X'; state3.s[1][2] = 'O';
+            state3.s[2][0] = 'X'; state3.s[2][1] = ' '; state3.s[2][2] = ' ';
+            EXPECT_EQ(state3.getScoreFinalState(state3.s), 1);
+        }
+    }
 
+    void TestGetScoreState(){
+        GameState finalState;
+            finalState.s[0][0] = 'X'; finalState.s[0][1] = 'X'; finalState.s[0][2] = 'X';
+            finalState.s[1][0] = 'X'; finalState.s[1][1] = 'O'; finalState.s[1][2] = 'O';
+            finalState.s[2][0] = ' '; finalState.s[2][1] = 'O'; finalState.s[2][2] = ' ';
+        {
+            GameState state;
+            state = finalState;
+            EXPECT_EQ(state.getScore(state.s, true, 100, -100), std::make_pair(1, state.s));
+        }
+        {
+            GameState state1;
+            state1.s[0][0] = 'X'; state1.s[0][1] = ' '; state1.s[0][2] = 'X';
+            state1.s[1][0] = 'X'; state1.s[1][1] = 'O'; state1.s[1][2] = 'O';
+            state1.s[2][0] = ' '; state1.s[2][1] = 'O'; state1.s[2][2] = ' ';
+            EXPECT_EQ(state1.getScore(state1.s, true, 1, 0), std::make_pair(1, finalState.s));
+        }
+        {
+            GameState state2;
+            state2.s[0][0] = 'X'; state2.s[0][1] = 'X'; state2.s[0][2] = ' ';
+            state2.s[1][0] = 'X'; state2.s[1][1] = 'O'; state2.s[1][2] = 'O';
+            state2.s[2][0] = ' '; state2.s[2][1] = 'O'; state2.s[2][2] = ' ';
+            EXPECT_EQ(state2.getScore(state2.s, false, 1, 1), std::make_pair(1, finalState.s));
+        }
+
+        {
+            GameState state3;
+            state3.s[0][0] = 'X'; state3.s[0][1] = 'X'; state3.s[0][2] = 'O';
+            state3.s[1][0] = 'X'; state3.s[1][1] = 'O'; state3.s[1][2] = ' ';
+            state3.s[2][0] = ' '; state3.s[2][1] = ' '; state3.s[2][2] = ' ';
+            GameState finalState1;
+            finalState1.s[0][0] = 'X'; finalState1.s[0][1] = 'X'; finalState1.s[0][2] = 'O';
+            finalState1.s[1][0] = 'X'; finalState1.s[1][1] = 'O'; finalState1.s[1][2] = ' ';
+            finalState1.s[2][0] = 'O'; finalState1.s[2][1] = ' '; finalState1.s[2][2] = ' ';
+            EXPECT_EQ(state3.getScore(state3.s, false, -100, 100), std::make_pair(-1, finalState1.s));
+        }
+    }
 private:
     GameState state;
 };
@@ -108,4 +176,11 @@ TEST_F(GameStateTest, checkAndProcessFinalState) {
 
 TEST_F(GameStateTest, screen) {
     TestScreen();
+}
+
+TEST_F(GameStateTest, scoreFinalState) {
+    TestScoreFinalState();
+}
+TEST_F(GameStateTest, getScoreState) {
+    TestGetScoreState();
 }
